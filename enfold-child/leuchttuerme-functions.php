22f6b692852dac14d599a95d4151b9702f7980d8
R. Ehm <raphael.ehm@ip2c.de>
2023-07-28 18:12:44 +0200

    Update leuchttuerme-functions.php

--- enfold-child/leuchttuerme-functions.php
+++ enfold-child/leuchttuerme-functions.php
@@ -12,8 +12,6 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 
 
-
-
 // https://kriesi.at/documentation/enfold/hooks-and-filters/
 /*	ava_main_header
 	ava_main_header_sidebar
